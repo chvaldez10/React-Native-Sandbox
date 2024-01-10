b79bc0878eb920ee998e94ac1a7f726ca8815d4c
chvaldez10 <christianvaldez021@gmail.com>
2024-01-10 16:47:54 -0700

    added tabbar

---
+++ AribnbClone/app/(tabs)/trips.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const Page = () => {
+  return (
+    <View>
+      <Text>trip</Text>
+    </View>
+  );
+};
+
+export default Page;
