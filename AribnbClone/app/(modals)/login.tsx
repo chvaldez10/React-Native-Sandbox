eb2d0d8fd9770c5fbe776f7def4252175cf08826
chvaldez10 <christianvaldez021@gmail.com>
2024-01-10 17:16:18 -0700

    added modals

--- AribnbClone/app/(modals)/login.tsx
+++ AribnbClone/app/(modals)/login.tsx
@@ -1,12 +1,10 @@
 import { View, Text } from "react-native";
 import React from "react";
-import { Link } from "expo-router";
 
 const Page = () => {
   return (
     <View>
-      <Link href={"/(modals)/login"}>login</Link>
-      <Link href={"/(modals)/booking"}>booking</Link>
+      <Text>login</Text>
     </View>
   );
 };
