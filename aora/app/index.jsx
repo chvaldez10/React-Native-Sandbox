783268fd518d75e7bf8b2451a32bfe965173db16
chvaldez10 <christianvaldez021@gmail.com>
2025-02-01 11:47:39 -0700

    add tailwind

--- aora/app/index.jsx
+++ aora/app/index.jsx
@@ -4,24 +4,12 @@ import { Link } from "expo-router";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello World</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-black">Hello World</Text>
       <StatusBar style="auto" />
-      <Link href="/profile" style={styles.link}>
+      <Link href="/profile" className="text-blue-500">
         Go to profile
       </Link>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  link: {
-    color: "blue",
-  },
-});
