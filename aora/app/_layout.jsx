3ebaa1629ebee6fdd348afe0fcb9e924b6152616
chvaldez10 <christianvaldez021@gmail.com>
2025-02-16 20:16:48 -0700

    add search

--- aora/app/_layout.jsx
+++ aora/app/_layout.jsx
@@ -37,7 +37,10 @@ const RootLayout = () => {
   return (
     <GlobalProvider>
       <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="index" />
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="search/[query]" options={{ headerShown: false }} />
       </Stack>
     </GlobalProvider>
   );
